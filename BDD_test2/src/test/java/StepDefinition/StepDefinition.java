573c3c85edfbe2d612ae009e2a4829064d6ffaf7
IBM <IBM@DEVOPS_10>
2018-07-31 11:30:51 +0530

    v iy i o

--- BDD_test2/src/test/java/StepDefinition/StepDefinition.java
+++ BDD_test2/src/test/java/StepDefinition/StepDefinition.java
@@ -15,7 +15,7 @@ String title;
 public void login() {
 	System.setProperty("webdriver.gecko.driver","E:\\gecko\\geckodriver.exe");
 	driver= new FirefoxDriver();
-	  driver.get("http://ec2-18-217-71-1.us-east-2.compute.amazonaws.com:8090/TestWebapp/");
+	  driver.get("http://ec2-18-220-4-236.us-east-2.compute.amazonaws.com:8090/TestWebapp/");
       driver.manage().window().maximize();
 	
     // Write code here that turns the phrase above into concrete actions
